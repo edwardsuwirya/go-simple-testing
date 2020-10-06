deba9487f8be19d0722e150ca89129c2a15e536c
edwardsuwirya <edward.suwirya@ffi.co.id>
2020-10-06 15:09:32 +0700

    Using testify

--- services/calc.go
+++ services/calc.go
@@ -1,12 +1,17 @@
 package services
 
+import "errors"
+
 type Calculator struct {
 	Num1 int
 	Num2 int
 }
 
-func (c *Calculator) Add() int {
-	return c.Num1 + c.Num2
+func (c *Calculator) Add() (int, error) {
+	if c.Num1 < 0 || c.Num2 < 0 {
+		return -1, errors.New("Tidak boleh negatif")
+	}
+	return c.Num1 + c.Num2, nil
 }
 
 func (c *Calculator) Sub() int {
--- services/calc_test.go
+++ services/calc_test.go
@@ -1,9 +1,12 @@
 package services
 
-import "testing"
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
 
 func TestCalculator_Add(t *testing.T) {
-	t.Run("Calculator Add operator testing", func(t *testing.T) {
+	t.Run("Calculator Add operator testing, it should return int", func(t *testing.T) {
 		numSample1 := 10
 		numSample2 := 20
 
@@ -11,9 +14,22 @@ func TestCalculator_Add(t *testing.T) {
 			Num1: numSample1,
 			Num2: numSample2,
 		}
-		if calc.Add() != 30 {
-			t.Error("It should return 30")
+		result, err := calc.Add()
+		assert.Nil(t, err)
+		assert.Equal(t, 30, result)
+	})
+
+	t.Run("It should return error when receive negative value", func(t *testing.T) {
+		numSample1 := -1
+		numSample2 := 10
+		calc := Calculator{
+			Num1: numSample1,
+			Num2: numSample2,
 		}
+		result, err := calc.Add()
+
+		assert.NotNil(t, err)
+		assert.Equal(t, -1, result)
 	})
 }
 func TestCalculator_Sub(t *testing.T) {
@@ -25,8 +41,7 @@ func TestCalculator_Sub(t *testing.T) {
 			Num1: numSample1,
 			Num2: numSample2,
 		}
-		if calc.Sub() != 1 {
-			t.Error("It should return 1")
-		}
+		result := calc.Sub()
+		assert.Equal(t, 1, result)
 	})
 }
