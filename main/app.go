05f196385fef96c71539081b21227474628c35c6
edwardsuwirya <edward_suwirya@yahoo.com>
2022-09-29 12:33:43 +0700

    Add REST API

--- main/app.go
+++ main/app.go
@@ -1,48 +1,71 @@
 package main
 
 import (
-	"flag"
-	"gosimpletest/helpers"
+	"fmt"
+	"github.com/gin-gonic/gin"
 	"gosimpletest/services"
+	"net/http"
+	"os"
+	"strconv"
 )
 
 func main() {
-	num1 := flag.Int("num1", 0, "first number")
-	num2 := flag.Int("num2", 0, "second number")
-	opr := flag.String("opr", "add", "Calculator Operator")
-	flag.Parse()
-	showResult := helpers.Output{}
-	var result = 0
-	switch *opr {
-	case "add":
-		myCalc := services.Calculator{
-			*num1, *num2,
-		}
-		res, err := myCalc.Add()
+	port := os.Getenv("PORT")
+	r := gin.Default()
+	r.GET("/calc", func(c *gin.Context) {
+		sNum1 := c.DefaultQuery("num1", "0")
+		sNum2 := c.DefaultQuery("num2", "0")
+		opr := c.DefaultQuery("opr", "add")
+
+		num1, err := strconv.Atoi(sNum1)
+		num2, err := strconv.Atoi(sNum2)
 		if err != nil {
-			err := showResult.Error(err)
-			if err != nil {
-				return
+			c.JSON(http.StatusBadRequest, gin.H{
+				"message": "Unknown number",
+			})
+			return
+		}
+		var result = 0
+		switch opr {
+		case "add":
+			myCalc := services.Calculator{
+				num1, num2,
 			}
-		} else {
-			result = res
-		}
-	case "sub":
-		myCalc := services.Calculator{
-			*num1, *num2,
-		}
-		res, err := myCalc.Sub()
-		if err != nil {
-			err := showResult.Error(err)
+			res, err := myCalc.Add()
 			if err != nil {
+				c.JSON(http.StatusUnprocessableEntity, gin.H{
+					"message": err,
+				})
 				return
+			} else {
+				result = res
 			}
-		} else {
-			result = res
+		case "sub":
+			myCalc := services.Calculator{
+				num1, num2,
+			}
+			res, err := myCalc.Sub()
+			if err != nil {
+				c.JSON(http.StatusUnprocessableEntity, gin.H{
+					"message": err,
+				})
+				return
+			} else {
+				result = res
+			}
+		default:
+			c.JSON(http.StatusUnprocessableEntity, gin.H{
+				"message": "Unknown operator",
+			})
+			return
 		}
-	}
-	err := showResult.Console(result)
+		c.JSON(http.StatusOK, gin.H{
+			"message": result,
+		})
+	})
+	err := r.Run(fmt.Sprintf(":%s", port))
 	if err != nil {
-		return
+		panic("Can not run server")
 	}
+
 }
